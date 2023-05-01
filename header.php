ad82c0180e794cba93735671c978b765efb47685
jangel01 <jasonangel@csu.fullerton.edu>
2023-05-01 10:33:56 -0700

    .

--- header.php
+++ header.php
@@ -24,8 +24,8 @@
                 </a>
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Schedule Meeting</a></li>
+                    <li><a id = "home" href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                    <li><a id = "schedule-meeting" href="#" class="nav-link px-2 text-white">Schedule Meeting</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">Search Meeting</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
                 </ul>
---
+++ schedule-meeting.php
@@ -0,0 +1,5 @@
+<?php include_once 'header.php'; ?>
+
+<h1 class="text-danger">lo</h1>
+
+<?php include_once 'footer.php'; ?>
