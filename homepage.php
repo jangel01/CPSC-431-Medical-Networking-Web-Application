68ac57da72bd2cdde3fec6445e14e1a627e8185e
jangel01 <jasonangel@csu.fullerton.edu>
2023-05-02 22:11:28 -0700

    .

--- homepage.php
+++ homepage.php
@@ -1,6 +1,66 @@
 <?php include_once 'header.php'; ?>
 
-<h1 class="text-danger">lo</h1>
+<div class="container mt-5">
+    <div class="row gx-3">
+        <div class="col-10">
+            <h2> Upcoming Meetings</h2>
+            <div class="table-responsive" style="overflow-x: auto; overflow-y: auto; max-height: 200px;">
+                <table class="table table-striped table-bordered">
+                    <thead class="text-bg-dark">
+                        <tr>
+                            <th>Initiator</th>
+                            <th>Time </th>
+                            <th>Location</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class = "text-decoration-underline" style = "cursor:pointer;">
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                        </tr>
+                        <tr class = "text-decoration-underline" style = "cursor:pointer;">
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                        </tr>
+                        <tr class = "text-decoration-underline" style = "cursor:pointer;">
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                        </tr>
+                        <tr class = "text-decoration-underline" style = "cursor:pointer;">
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                        </tr>
+                        <tr class = "text-decoration-underline" style = "cursor:pointer;">
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="col-2">
+            <h2>FAQ</h2>
+            <ul>
+                <li>
+                    <p> <u>How to get started?</u> </p>
+                </li>
+                <li>
+                    <p> <u> How to change preferences? </u></p>
+                </li>
+                <li>
+                    <p><u>How to schedule a meeting?</u> </p>
+                </li>
+            </ul>
+            <p> <u> View More... </u> </p>
+        </div>
+    </div>
+</div>
 
 
 <?php include_once 'footer.php'; ?>
