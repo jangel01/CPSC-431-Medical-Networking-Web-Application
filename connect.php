43fadda9de17d1d2202948b0c5c37485dde3ab84
jangel01 <jasonangel@csu.fullerton.edu>
2023-05-03 02:48:06 -0700

    .

--- connect.php
+++ connect.php
@@ -6,80 +6,81 @@
         <div class="col-12 mb-2">
             <h2>Search </h2>
         </div>
-        <div class="row">
-            <div class="col-2 mb-2">
-                <select class="form-select" id="role" name="role">
-                    <option value="speciality">Speciality</option>
-                    <option value="role">Medical professional</option>
-                    <option value="other">Industry</option>
-                    <option value="other">Location</option>
-                </select>
-            </div>
-            <div class="col-10 mb-2">
-                <form>
-                    <div class="form-group">
-                        <div class = "input-group">
-
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search...">
-                            <button type="button" class="btn btn-dark">Search</button>
-                        </div>
-
-                    </div>
-                </form>
-
-            </div>
+    </div>
+    <div class="row">
+        <div class="col-2 mb-2">
+            <select class="form-select" id="role" name="role">
+                <option value="speciality">Speciality</option>
+                <option value="role">Medical professional</option>
+                <option value="other">Industry</option>
+                <option value="other">Location</option>
+            </select>
         </div>
+        <div class="col-10 mb-2">
+            <form>
+                <div class="form-group">
+                    <div class="input-group">
 
-        <div class="row">
-            <div class="col-12">
-                <h2 class="mb-"> Search Results</h2>
-                <div class="table-responsive" style="overflow-x: auto;">
-                    <table class="table table-striped table-bordered">
-                        <thead class="text-bg-dark">
-                            <tr>
-                                <th>Name</th>
-                                <th>Speciality</th>
-                                <th>Location </th>
-                                <th>Food preferences</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="text-decoration-underline" style="cursor:pointer;">
-                                <td>Placeholder text</td>
-                                <td class="bg-warning-subtle">Placeholder text</td>
-                                <td>Placeholder text</td>
-                                <td>Placeholder text</td>
-                            </tr>
-                            <tr class="text-decoration-underline" style="cursor:pointer;">
-                                <td>Placeholder text</td>
-                                <td class="bg-warning-subtle">Placeholder text</td>
-                                <td>Placeholder text</td>
-                                <td>Placeholder text</td>
-                            </tr>
-                            <tr class="text-decoration-underline" style="cursor:pointer;">
-                                <td>Placeholder text</td>
-                                <td class="bg-warning-subtle">Placeholder text</td>
-                                <td>Placeholder text</td>
-                                <td>Placeholder text</td>
-                            </tr>
-                            <tr class="text-decoration-underline" style="cursor:pointer;">
-                                <td>Placeholder text</td>
-                                <td class="bg-warning-subtle">Placeholder text</td>
-                                <td>Placeholder text</td>
-                                <td>Placeholder text</td>
-                            </tr>
-                            <tr class="text-decoration-underline" style="cursor:pointer;">
-                                <td>Placeholder text</td>
-                                <td class="bg-warning-subtle">Placeholder text</td>
-                                <td>Placeholder text</td>
-                                <td>Placeholder text</td>
-                            </tr>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search...">
+                        <button type="button" class="btn btn-dark">Search</button>
+                    </div>
 
-                        </tbody>
-                    </table>
                 </div>
-            </div>
+            </form>
+
         </div>
     </div>
 
-    <?php include_once 'footer.php'; ?>
+    <div class="row">
+        <div class="col-12">
+            <h2 class="mb-2"> Search Results</h2>
+            <div class="table-responsive" style="overflow-x: auto;">
+                <table class="table table-striped table-bordered">
+                    <thead class="text-bg-dark">
+                        <tr>
+                            <th>Name</th>
+                            <th>Speciality</th>
+                            <th>Location </th>
+                            <th>Food preferences</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td class="bg-warning-subtle">Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td class="bg-warning-subtle">Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td class="bg-warning-subtle">Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td class="bg-warning-subtle">Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td class="bg-warning-subtle">Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include_once 'footer.php'; ?>
--- manage-meetings.php
+++ manage-meetings.php
@@ -1,7 +1,113 @@
 <?php include_once 'header.php'; ?>
 
-<h1 class="text-danger">lo</h1>
+
+<div class="container mt-5 mb-5">
+
+    <div class="row">
+        <div class="col-12">
+            <h2 class="mb-5"> Incoming Requests</h2>
+
+            <h4>Medical Professionals</h4>
+            <div class="table-responsive mb-4" style="overflow-x: auto;">
+                <table class="table table-striped table-bordered">
+                    <thead class="text-bg-dark">
+                        <tr>
+                            <th>Name</th>
+                            <th>Role</th>
+                            <th> Speciality</th>
+                            <th> Practice Name</th>
+                            <th> Practice Location</th>
+                            <th>Availability</th>
+                            <th>Food preferences</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
 
 
+                    </tbody>
+                </table>
+            </div>
+
+            <h4>Medical Company</h4>
+            <div class="table-responsive" style="overflow-x: auto;">
+                <table class="table table-striped table-bordered">
+                    <thead class="text-bg-dark">
+                        <tr>
+                            <th>Company Name</th>
+                            <th>Speciality</th>
+                            <th> Practice Name</th>
+                            <th> Practice Location</th>
+                            <th>Availability</th>
+                            <th>Food preferences</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+                        <tr class="text-decoration-underline" style="cursor:pointer;">
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                            <td>Placeholder text</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include_once 'footer.php'; ?>
