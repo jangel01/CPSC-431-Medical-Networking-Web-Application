f1ca61fe6478be867a18c04abb48e9270388708a
jangel01 <jasonangel@csu.fullerton.edu>
2023-05-08 22:34:56 -0700

    .

--- includes/initial.preferences.php
+++ includes/initial.preferences.php
@@ -1,19 +1,37 @@
-<?php 
+<?php
 
 session_start();
 $userId = $_SESSION['user_id'];
 $userType = $_SESSION['user_type'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include "../classes/dbh.classes.php";
+    include "../classes/preferences.classes.php";
+    include "../classes/preferences.contr.classes.php";
+    
     if (isset($_POST['availability'])) {
         $availability = $_POST['availability'];
         $availability = implode(", ", $availability);
-        
-    } 
-    
+
+        $initPreferences = new PreferencesContr($userId, $userType, $availability);
+
+        // set availability preferences
+        $initPreferences->setAvailabilityPreferencesContr();
+
+        // redirect to next page
+        header("location: ../homepage.php");
+    }
+
     if (isset($_POST['food'])) {
         $food = $_POST['food'];
         $food = implode(", ", $food);
-    } 
 
+        $initPreferences = new PreferencesContr($userId, $userType, null, $food);
+
+        // set food preferences
+        $initPreferences->setFoodPreferencesContr();
+    }
+
+    // redirect to next page
+    header("location: ../homepage.php");
 }
